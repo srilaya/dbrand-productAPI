978b1c5208309bcc7a53f623922706ab9c713166
srilaya <srilaya.y@gmail.com>
2018-05-16 14:42:29 -0400

    auto-increment added

--- src/main/java/com/dbrand/store/products/Product.java
+++ src/main/java/com/dbrand/store/products/Product.java
@@ -1,22 +1,24 @@
 package com.dbrand.store.products;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Product {
 
-	String name;
-	String type;
-	long id;
-	String description;
+	private static final AtomicInteger count = new AtomicInteger(0); 
+	private long id;
+	private String name;
+	private String type;
+	private String description;
 	
 	public Product() {	
-		
 	}
 	
-	public Product(String name, String type, long id, String description) {
+	public Product(String name, String type, String description) {
 		
 		this.name = name;
 		this.type = type;
-		this.id = id;
 		this.description = description;
+		id = count.incrementAndGet();
 	}
 	
 	public String getName() {
@@ -35,9 +37,7 @@ public class Product {
 	public long getId() {
 		return id;
 	}
-	public void setId(long id) {
-		this.id = id;
-	}
+	
 	public String getDescription() {
 		return description;
 	}
