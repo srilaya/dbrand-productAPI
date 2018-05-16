093bed43e3876a2db9d9d68ef1603ed9ba0379ff
srilaya <srilaya.y@gmail.com>
2018-05-16 14:32:41 -0400

    first commit

---
+++ src/main/java/com/dbrand/store/ProductsApiDataApplication.java
@@ -0,0 +1,12 @@
+package com.dbrand.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApiDataApplication.class, args);
+	}
+}
